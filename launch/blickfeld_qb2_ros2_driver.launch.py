cb340d73ca391f1253cd2561019397e9101802b5
Gunreben <gunreben@wifa.uni-leipzig.de>
2025-07-10 12:13:30 +0200

    reworked launchfile

--- launch/blickfeld_qb2_ros2_driver.launch.py
+++ launch/blickfeld_qb2_ros2_driver.launch.py
@@ -3,9 +3,30 @@
 from launch import LaunchDescription
 from launch_ros.descriptions import ComposableNode
 from launch_ros.actions import ComposableNodeContainer
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+    # Declare launch arguments
+    fqdn_arg = DeclareLaunchArgument(
+        'fqdn',
+        default_value='192.168.26.26',
+        description='FQDN or IP address of the Qb2 device'
+    )
+    
+    frame_id_arg = DeclareLaunchArgument(
+        'frame_id',
+        default_value='Root',
+        description='Frame ID for the point cloud'
+    )
+    
+    point_cloud_topic_arg = DeclareLaunchArgument(
+        'point_cloud_topic',
+        default_value='/bf/points_raw',
+        description='Topic name for publishing point cloud'
+    )
+
     container = ComposableNodeContainer(
         name="blickfeld_qb2_component",
         namespace="",
@@ -18,18 +39,23 @@ def generate_launch_description():
                 name="blickfeld_qb2_driver",
                 parameters=[
                     {
-                        "fqdn": "qb2",
-                        "serial_number": "XXXXXXXXX",
-                        "application_key": "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY",
-                        "frame_id": "lidar",
-                        "point_cloud_topic": "/bf/points_raw",
+                        "fqdn": LaunchConfiguration('fqdn'),
+                        "frame_id": LaunchConfiguration('frame_id'),
+                        "point_cloud_topic": LaunchConfiguration('point_cloud_topic'),
                         "use_measurement_timestamp": False,
                         "publish_intensity": True,
                         "publish_point_id": True,
                     }
                 ],
+                extra_arguments=[{'use_intra_process_comms': True}],
             ),
         ],
         output="screen",
     )
-    return LaunchDescription([container])
+    
+    return LaunchDescription([
+        fqdn_arg,
+        frame_id_arg,
+        point_cloud_topic_arg,
+        container
+    ])
